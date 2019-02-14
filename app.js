9e3ac7bac543caa42535e32c7d81cf09311ab1e7
Tyler <tylerthomasallen@gmail.com>
2019-02-14 14:10:01 -0800

    Connect to mongo

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+const db = require('./config/keys').mongoURI;
+const mongoose = require('mongoose');
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err))
 
 app.get("/", (req, res) => res.send('Hello, World'));
-
-
 
 const port = process.env.PORT || 5000;
 
